cfa43aea5dfe8621febca3aa3311114af9b80460
bellaacm <martosb@wsc.school.nz>
2026-05-05 14:54:20 +1200

    learned basic print functions and how to do ascii art

---
+++ Lesson 1 - output and input/ascii_art.py
@@ -0,0 +1,11 @@
+### ASCII Art ###
+# Create a picture using lines, spaces and/or some letters.
+# You'll need to use print multiple times to create your picture.
+print(" ╱|、")  
+print("(˚ˎ 。7")  
+print(" |、˜〵 ")         
+print(" じしˍ,)ノ")
+# -------------------------
+
+# EXTENSION
+# Try to create a more complicated ASCII picture
---
+++ _main.py
@@ -0,0 +1,7 @@
+print("Hello")
+print()
+print("hello")
+print("hello")
+print("i'm coding")
+print("i'm coding")
+      

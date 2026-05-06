5dcd37f3e27437ee0676fec8d86ed2a7ebcfead9
bellaacm <martosb@wsc.school.nz>
2026-05-07 09:14:06 +1200

    guessing game code

--- Lesson 1 - output and input/guessing_game.py
+++ Lesson 1 - output and input/guessing_game.py
@@ -1,13 +1,13 @@
 ### Create a 3-part guessing game ###
 
 # Give your user the first hint and wait for input
-
+input("I am a master architect with no hands What am I?")
 # Give your user the second hint and wait for input
-
+input("I have 8 knees but no elbows. What am I?")
 # Give your user the final hint and wait for input
-
+input("I build a home that is also a trap. What am I?")
 # Tell your user the answer
-
+print("A spider.")
 
 # ------------------------------
 

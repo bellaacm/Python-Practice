edf0dfdf70762d092289b85e80ea387bd2a8824a
bellaacm <martosb@wsc.school.nz>
2026-05-07 10:08:51 +1200

    practicing different data types

--- _main.py
+++ _main.py
@@ -24,3 +24,10 @@ print("my name is", name)
 name = "Hellen"
 age = 15
 print(name, "is", age)
+
+age = 5 
+print("He is " + str(age))
+
+
+age = input("Age?")
+print("Birth year:", 2026 - int(age) )

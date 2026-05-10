ce827331532c034e175eeb98cca33ebce69dcc9b
bellaacm <martosb@wsc.school.nz>
2026-05-11 11:51:50 +1200

    completed seconds calculator task

--- Lesson 1 - output and input/Lesson 2 - Data types and Variables/seconds_calculator.py
+++ Lesson 1 - output and input/Lesson 2 - Data types and Variables/seconds_calculator.py
@@ -2,23 +2,19 @@
 # and outputs how many seconds in that number of days
 
 # Values - start by writing constants to hold:
-# The number of seconds in a minute
-
-# The number of minutes in an hour
-
-# The number of hours in a day
-
+# The number of seconds in a day
+seconds = 86400
 
 # Get input from the user and save it in a variable
-
+days_input = input ("How many days?")
 # Change the value into an integer and resave in the variable
-
-
+days = int(days_input)
 # Calculate the number of seconds using * with the input and your constants. 
+total = seconds * days
 # Save it in a new variable.
 
 # Output the answer
-
+print ( total, "seconds")
 # ---------------------------------
 
 # EXTENSION

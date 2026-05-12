7ff7f310ae052cf6e76d46bfeab9118553897bf1
bellaacm <martosb@wsc.school.nz>
2026-05-12 15:06:19 +1200

    quiz

---
+++ quiz.py
@@ -0,0 +1,75 @@
+### quiz ###
+# this is a 10 question quiz involving a large variety of topics 
+
+# introduction 
+name = input("What is your name?")
+print("hello", name, "welcome to this quiz!")
+print("This will be a 10 question quiz assesing your general knowledge")
+
+# question 1
+print("First question:")
+first_question = input("What is the worlds tallest building?")
+first_question_answer = print("As of 2026, the worlds tallest building is The Burj Khalifa in Dubai")
+
+# question 2
+print("Second question:")
+second_question = input("What is the largest ocean on earth?")
+second_question_answer = print("The Pacific Ocean")
+
+# question 3
+print("Third question:")
+third_question = input("Who was the first person to walk on the moon?")
+third_question_answer = print("Neil Armstrong")
+
+# question 4
+print("Fourth question:")
+fourth_question = input("Which superhero is known as the 'Man of Steel'?")
+fourth_question_answer = print("Superman")
+
+# question 5
+print("Fifth question:")
+fifth_question = input("What is the highest rated TV show?")
+fifth_question_answer = print("Breaking Bad")
+
+# question 6
+print("Sixth question:")
+Sixth_question = input("How many days does it take for the Earth to orbit the Sun?")
+Sixth_question_answer = print("365 days")
+
+# question 7
+print("Seventh question:")
+Seventh_question = input("What are the two national animals of Australia?")
+Seventh_question_answer = print("The red kangaroo and the emu")
+
+# question 8
+print("Eighth question:")
+Eighth_question = input("Which artist painted the ceiling of the Sistine Chapel in Rome?")
+Eighth_question_answer = print("Michelangelo")
+
+# question 9 (!)
+print("Ninth question:")
+Ninth_question = input("")
+Ninth_question_answer = print("")
+
+# question 10 (!)
+print("Tenth question:")
+Tenth_question = input("")
+Tenth_question_answer = print("")
+
+# outro
+print("This is the end of the quiz.")
+input("How many questions did you answer correctly?")
+print("Congratulations! Thank you", name, "for participating in this quiz :)" )
+
+
+
+
+
+
+
+
+
+
+
+
+

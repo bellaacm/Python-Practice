bfa93b090a036c8903723cf4fe681fa4efd453f9
bellaacm <martosb@wsc.school.nz>
2026-05-14 10:29:13 +1200

    edited quiz

--- quiz.py
+++ quiz.py
@@ -4,57 +4,78 @@
 # introduction 
 name = input("What is your name?")
 print("hello", name, "welcome to this quiz!")
-print("This will be a 10 question quiz assesing your general knowledge")
+print("This will be a 10 question quiz assesing your general knowledge.")
+
 
 # question 1
 print("First question:")
 first_question = input("What is the worlds tallest building?")
-first_question_answer = print("As of 2026, the worlds tallest building is The Burj Khalifa in Dubai")
+if first_question == 'Burj Khalifa':
+   print("Good job!")
+first_question_answer = print("As of 2026, the worlds tallest building is The Burj Khalifa in Dubai.")
 
 # question 2
 print("Second question:")
-second_question = input("What is the largest ocean on earth?")
-second_question_answer = print("The Pacific Ocean")
+second_question = input("What is the largest ocean on Earth?")
+if second_question == 'Pacific Ocean':
+   print("Good job!")
+second_question_answer = print("The largest ocean on Earth is the Pacific Ocean.")
 
 # question 3
 print("Third question:")
 third_question = input("Who was the first person to walk on the moon?")
-third_question_answer = print("Neil Armstrong")
+if third_question == 'Neil Armstrong':
+   print("Good job!")
+third_question_answer = print("The first person to walk on the moon was Neil Armstrong.")
 
 # question 4
 print("Fourth question:")
 fourth_question = input("Which superhero is known as the 'Man of Steel'?")
-fourth_question_answer = print("Superman")
+if fourth_question == 'Superman':
+   print("Good job!")
+fourth_question_answer = print("Superman is known as the 'Man of Steel'.")
 
 # question 5
 print("Fifth question:")
 fifth_question = input("What is the highest rated TV show?")
-fifth_question_answer = print("Breaking Bad")
+if fifth_question == 'Breaking Bad':
+   print("Good job!")
+fifth_question_answer = print("The highest rated TV show is Breaking Bad.")
 
 # question 6
 print("Sixth question:")
 Sixth_question = input("How many days does it take for the Earth to orbit the Sun?")
-Sixth_question_answer = print("365 days")
+if Sixth_question == '365':
+   print("Good job!")
+Sixth_question_answer = print("It takes 365 days for the Earth to orbit the Sun.")
 
 # question 7
 print("Seventh question:")
 Seventh_question = input("What are the two national animals of Australia?")
-Seventh_question_answer = print("The red kangaroo and the emu")
+if Seventh_question == 'Red kangaroo and emu':
+   print("Good job!")
+Seventh_question_answer = print("The red kangaroo and the emu are the two national animals of Austrailia.")
 
 # question 8
 print("Eighth question:")
 Eighth_question = input("Which artist painted the ceiling of the Sistine Chapel in Rome?")
-Eighth_question_answer = print("Michelangelo")
+if Eighth_question == 'Michelangelo':
+   print("Good job!")
+Eighth_question_answer = print("Michelangelo painted the ceiling of the Sistine Chapel in Rome.")
 
 # question 9 (!)
 print("Ninth question:")
-Ninth_question = input("")
-Ninth_question_answer = print("")
+Ninth_question = input("What is the only body part fully grown from birth?")
+if Ninth_question == 'Eyes':
+   print("Good job!")
+Ninth_question_answer = print("The only body part fully grown from brith are your eyes.")
 
 # question 10 (!)
 print("Tenth question:")
-Tenth_question = input("")
-Tenth_question_answer = print("")
+Tenth_question = input("What are a groups of crows called?")
+if Tenth_question == 'Murder':
+   print("Good job!")
+Tenth_question_answer = print("A group of crows are famously reffered to as a 'murder'.")
 
 # outro
 print("This is the end of the quiz.")
